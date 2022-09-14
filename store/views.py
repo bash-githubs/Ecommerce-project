5431df4ff044589fee373f4390b4c1bdfb32fc33
Joseph Bashorun <josephbashorun5@gmail.com>
2022-09-14 20:49:39 +0100

    fixed the paginator

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from cart.models import CartItem
 from .models import Product
 from category.models import Category
 from django.http import HttpResponse
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
     
@@ -15,15 +15,21 @@ def store(request, category_slug=None):
     if category_slug!=None:
         categories=get_object_or_404(Category, slug=category_slug)
         products=Product.objects.filter(category=categories, is_available=True)
+        paginator=Paginator(products, 3)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         products_count=products.count()
     
     else:
 
         products=Product.objects.all().filter(is_available=True)
+        paginator=Paginator(products, 3)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         products_count=products.count()
     
     context={
-        'products':products,
+        'products':paged_products,
         'products_count':products_count,
 
     }
